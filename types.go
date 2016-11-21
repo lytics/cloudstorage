43bfff5af8472d6ac68c3b6e009ee39d4b498fbe
Aaron Raddon <aaron@lytics.io>
2016-11-21 11:01:56 -0800

    Doc updates

--- types.go
+++ types.go
@@ -5,63 +5,56 @@ import (
 	"fmt"
 )
 
+// TokenSource Is the source/location/type of token
 type TokenSource string
 
+// AccessLevel is the level of permissions on files
 type AccessLevel int
 
 const (
+	// Authentication Source
 	LyticsJWTKeySource   TokenSource = "LyticsJWTkey"
 	GoogleJWTKeySource   TokenSource = "GoogleJWTFile"
 	GCEMetaKeySource     TokenSource = "gcemetadata"
 	LocalFileSource      TokenSource = "localfiles"
 	GCEDefaultOAuthToken TokenSource = "gcedefaulttoken"
 
+	// File Permissions Levels
 	ReadOnly  AccessLevel = 0
 	ReadWrite AccessLevel = 1
 )
 
-//Objects are just a collection of Object(s).  Used as the results for store.List commands.
+// ObjectIterator
+type ObjectIterator interface {
+}
+
+// Objects are just a collection of Object(s).  Used as the results for store.List commands.
 type Objects []Object
 
 func (o Objects) Len() int           { return len(o) }
 func (o Objects) Less(i, j int) bool { return o[i].Name() < o[j].Name() }
 func (o Objects) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
 
-// the cloud store config parameters
-//
-//   # General settings
-//   LogggingContext: logging context that's prefix to each logline.
-//   TokenSource    : the methods of accessing the event archive.
-//                    valid options are the xxxSource const above.
-//   TmpDir         : where to cache local copies of the event files.
-//   FilterStreams  : list of streams to filter out during archiving.
-//   Topic          : the topic name to use when looking for archived events.
-//                    all other topics will be assumed to be streaming only topics.
-//
-//   # GCS Archive settings
-//   Project        :
-//   Bucket         :
-//   JwtConf        : required if TokenSource is JWTKey
-//   PageSize       : what page size to use with Google
-//
-//   # Local Archive settings
-//   LocalFS        : the location to use for archived events (i.e. the mocked cloud)
-
+// CloudStoreContext the cloud store config parameters
 type CloudStoreContext struct {
+	// logging context that's prefix to each logline.
 	LogggingContext string
-
+	// the methods of accessing the event archive. valid options are the xxxSource const above.
 	TokenSource TokenSource
 
-	//GCS Archive
-	Project  string
-	Bucket   string
-	PageSize int // the page size to use with google api requests (default 1000)
+	// GCS Settings (project, bucket)
+	Project string
+	Bucket  string
+
+	// the page size to use with google api requests (default 1000)
+	PageSize int
 
 	// used by LyticsJWTKeySource
 	JwtConf *JwtConf
 	// used by GoogleJWTKeySource
 	JwtFile string
-	Scope   string
+	// Permissions scope
+	Scope string
 
 	//LocalFS Archive
 	LocalFS string // The location to use for archived events
@@ -70,7 +63,7 @@ type CloudStoreContext struct {
 	TmpDir string
 }
 
-//For use with google/google_jwttransporter.go
+// JwtConf For use with google/google_jwttransporter.go
 // Which can be used by the google go sdk's
 type JwtConf struct {
 	//below are the fields from a Google Compute Engine's Credentials json file.
@@ -82,6 +75,7 @@ type JwtConf struct {
 	Scopes            []string // what scope to use when the token is created.  for example https://github.com/google/google-api-go-client/blob/0d3983fb069cb6651353fc44c5cb604e263f2a93/storage/v1/storage-gen.go#L54
 }
 
+// Validate that this is a valid jwt conf set of tokens
 func (j *JwtConf) Validate() error {
 	//convert Private_keybase64 to bytes.
 	_, err := j.KeyBytes()
