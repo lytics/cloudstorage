3f88f4f03141f1a45eaaa3cb20e7a41b9e100b0b
Aaron Raddon <araddon@gmail.com>
2017-11-07 22:20:31 -0800

    Test improvements, for sub-folders

--- object_test.go
+++ object_test.go
@@ -3,6 +3,7 @@ package cloudstorage_test
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -10,7 +11,7 @@ import (
 	"testing"
 	"time"
 
-	"golang.org/x/net/context"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/api/iterator"
 
 	"github.com/lytics/cloudstorage"
@@ -26,43 +27,42 @@ import (
 export TESTINT=1
 
 */
+
 func TestBasicRW(t *testing.T) {
+
 	store := testutils.CreateStore(t)
 	testutils.Clearstore(t, store)
-	//
-	//Create a new object and write to it.
-	//
+
+	// Create a new object and write to it.
 	obj, err := store.NewObject("prefix/test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertT(t, obj != nil, "Should return new empty obj")
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, obj)
 
 	f, err := obj.Open(cloudstorage.ReadWrite)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertT(t, f != nil, "the file was nil")
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, f)
 
 	testcsv := "Year,Make,Model\n1997,Ford,E350\n2000,Mercury,Cougar\n"
 
 	w := bufio.NewWriter(f)
-	n, err := w.WriteString(testcsv)
-	testutils.AssertEq(t, nil, err, "error. %d", n)
+	_, err = w.WriteString(testcsv)
+	assert.Equal(t, nil, err)
 	w.Flush()
 
 	err = obj.Close()
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	//
-	//Read the object back out of the cloud storage.
-	//
+	// Read the object back out of the cloud storage.
 	obj2, err := store.Get("prefix/test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	f2, err := obj2.Open(cloudstorage.ReadOnly)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	bytes, err := ioutil.ReadAll(f2)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	testutils.AssertEq(t, testcsv, string(bytes))
+	assert.Equal(t, testcsv, string(bytes))
 }
 
 func TestAppend(t *testing.T) {
@@ -71,73 +71,87 @@ func TestAppend(t *testing.T) {
 	now := time.Now()
 	time.Sleep(10 * time.Millisecond)
 
-	//
-	//Create a new object and write to it.
-	//
+	// Create a new object and write to it.
 	obj, err := store.NewObject("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	f1, err := obj.Open(cloudstorage.ReadWrite)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertT(t, f1 != nil, "the file was nil")
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, f1)
 
 	testcsv := "Year,Make,Model\n2003,VW,EuroVan\n2001,Ford,Ranger\n"
 
 	w1 := bufio.NewWriter(f1)
-	n1, err := w1.WriteString(testcsv)
-	testutils.AssertEq(t, nil, err, "error. %d", n1)
+	_, err = w1.WriteString(testcsv)
+	assert.Equal(t, nil, err)
 	w1.Flush()
 
 	err = obj.Close()
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	//
 	// get the object and append to it...
-	//
 	morerows := "2013,VW,Jetta\n2011,Dodge,Caravan\n"
 	obj2, err := store.Get("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	// get updated time
 	updated := obj2.Updated()
-	testutils.AssertT(t, updated.After(now), "updated time was not set")
+	assert.True(t, updated.After(now), "updated time was not set")
 	time.Sleep(10 * time.Millisecond)
 
 	f2, err := obj2.Open(cloudstorage.ReadWrite)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertT(t, f2 != nil, "the file was nil")
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, f2)
 
 	w2 := bufio.NewWriter(f2)
-	n2, err := w2.WriteString(morerows)
-	testutils.AssertEq(t, nil, err, "error. %d", n2)
+	_, err = w2.WriteString(morerows)
+	assert.Equal(t, nil, err)
 	w2.Flush()
 
 	err = obj2.Close()
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	//
-	//Read the object back out of the cloud storage.
-	//
+	// Read the object back out of the cloud storage.
 	obj3, err := store.Get("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 	updated3 := obj3.Updated()
-	testutils.AssertT(t, updated3.After(updated), "updated time not updated")
+	assert.True(t, updated3.After(updated), "updated time not updated")
 	f3, err := obj3.Open(cloudstorage.ReadOnly)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	bytes, err := ioutil.ReadAll(f3)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	testutils.AssertEq(t, testcsv+morerows, string(bytes), "not the rows we expected.")
+	assert.Equal(t, testcsv+morerows, string(bytes), "not the rows we expected.")
 }
 
 func TestListObjsAndFolders(t *testing.T) {
 	store := testutils.CreateStore(t)
 	testutils.Clearstore(t, store)
-	//
+
+	createObjects := func(names []string) {
+		for _, n := range names {
+			obj, err := store.NewObject(n)
+			assert.Equal(t, nil, err)
+
+			f1, err := obj.Open(cloudstorage.ReadWrite)
+			assert.Equal(t, nil, err)
+			assert.NotEqual(t, nil, f1)
+
+			testcsv := "12345\n"
+
+			w1 := bufio.NewWriter(f1)
+			_, err = w1.WriteString(testcsv)
+			assert.Equal(t, nil, err)
+			w1.Flush()
+
+			err = obj.Close()
+			assert.Equal(t, nil, err)
+		}
+	}
+
 	// Create 5 objects in each of 3 folders
 	// ie 15 objects
-	//
 	folders := []string{"a", "b", "c"}
 	names := []string{}
 	for _, folder := range folders {
@@ -149,35 +163,23 @@ func TestListObjsAndFolders(t *testing.T) {
 
 	sort.Strings(names)
 
-	for _, n := range names {
-		obj, err := store.NewObject(n)
-		testutils.AssertEq(t, nil, err, "error. %v", obj)
-
-		f1, err := obj.Open(cloudstorage.ReadWrite)
-		testutils.AssertEq(t, nil, err, "error.")
-		testutils.AssertT(t, f1 != nil, "the file was nil")
-
-		testcsv := "12345\n"
-
-		w1 := bufio.NewWriter(f1)
-		n1, err := w1.WriteString(testcsv)
-		testutils.AssertEq(t, nil, err, "error. %d", n1)
-		w1.Flush()
-
-		err = obj.Close()
-		testutils.AssertEq(t, nil, err, "error.")
-	}
+	createObjects(names)
 
 	q := cloudstorage.NewQuery("list-test/")
 	q.Sorted()
 	objs, err := store.List(q)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 15, len(objs), "incorrect list len. wanted 15 got %d", len(objs))
 
-	testutils.AssertEq(t, 15, len(objs), "incorrect list len. wanted 15 got %d", len(objs))
+	q = cloudstorage.NewQuery("list-test/b")
+	q.Sorted()
+	objs, err = store.List(q)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 5, len(objs), "incorrect list len. wanted 5 got %d", len(objs))
 
 	for i, o := range objs {
 		t.Logf("%d found %v", i, o.Name())
-		testutils.AssertEq(t, names[i], o.Name(), "unexpected name.")
+		assert.Equal(t, names[i+5], o.Name(), "unexpected name.")
 	}
 
 	// Now with iterator
@@ -192,52 +194,74 @@ func TestListObjsAndFolders(t *testing.T) {
 		}
 		objs = append(objs, o)
 		t.Logf("%d found %v", i, o.Name())
-		testutils.AssertEq(t, names[i], o.Name(), "unexpected name.")
+		assert.Equal(t, names[i+5], o.Name(), "unexpected name.")
 		i++
 	}
 
-	testutils.AssertEq(t, 15, len(objs), "incorrect list len.")
+	assert.Equal(t, 5, len(objs), "incorrect list len.")
 
 	q = cloudstorage.NewQueryForFolders("list-test/")
 	folders, err = store.Folders(context.Background(), q)
 	t.Logf("folders %v", folders)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertEq(t, 3, len(folders), "incorrect list len. wanted 3 folders. ", folders)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 3, len(folders), "incorrect list len. wanted 3 folders. %v", folders)
+
+	folders = []string{"a/a2", "b/b1", "b/b2"}
+	names = []string{}
+	for _, folder := range folders {
+		for i := 0; i < 2; i++ {
+			n := fmt.Sprintf("list-test/%s/test%d.csv", folder, i)
+			names = append(names, n)
+		}
+	}
+
+	sort.Strings(names)
+
+	createObjects(names)
+
+	q = cloudstorage.NewQueryForFolders("list-test/")
+	folders, err = store.Folders(context.Background(), q)
+	t.Logf("folders %v", folders)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 3, len(folders), "incorrect list len. wanted 3 folders. %v", folders)
+
+	q = cloudstorage.NewQueryForFolders("list-test/b/")
+	folders, err = store.Folders(context.Background(), q)
+	t.Logf("folders %v", folders)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 2, len(folders), "incorrect list len. wanted 2 folders. %v", folders)
 }
 
 func TestTruncate(t *testing.T) {
 	store := testutils.CreateStore(t)
 	testutils.Clearstore(t, store)
-	//
-	//Create a new object and write to it.
-	//
+
+	// Create a new object and write to it.
 	obj, err := store.NewObject("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	f1, err := obj.Open(cloudstorage.ReadWrite)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertT(t, f1 != nil, "the file was nil")
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, f1, "the file was nil")
 
 	testcsv := "Year,Make,Model\n2003,VW,EuroVan\n2001,Ford,Ranger\n"
 
 	w1 := bufio.NewWriter(f1)
 	n1, err := w1.WriteString(testcsv)
-	testutils.AssertEq(t, nil, err, "error. %d", n1)
+	assert.Equal(t, nil, err, "error. %d", n1)
 	w1.Flush()
 
 	err = obj.Close()
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	//
-	//get the object and replace it...
-	//
+	// get the object and replace it...
 	newtestcsv := "Year,Make,Model\n2013,VW,Jetta\n"
 	obj2, err := store.Get("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	f2, err := obj2.Open(cloudstorage.ReadWrite)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertT(t, f2 != nil, "the file was nil")
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, f2, "the file was nil")
 
 	// Truncating the file will zero out the file
 	f2.Truncate(0)
@@ -246,71 +270,64 @@ func TestTruncate(t *testing.T) {
 
 	w2 := bufio.NewWriter(f2)
 	n2, err := w2.WriteString(newtestcsv)
-	testutils.AssertEq(t, nil, err, "error. %d", n2)
+	assert.Equal(t, nil, err, "error. %d", n2)
 	w2.Flush()
 
 	err = obj2.Close()
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	//
 	// Read the object back out of the cloud storage.
-	//
 	obj3, err := store.Get("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	f3, err := obj3.Open(cloudstorage.ReadOnly)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	bytes, err := ioutil.ReadAll(f3)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	testutils.AssertEq(t, newtestcsv, string(bytes), "not the rows we expected.")
+	assert.Equal(t, newtestcsv, string(bytes), "not the rows we expected.")
 }
 
 func TestNewObjectWithExisting(t *testing.T) {
 	store := testutils.CreateStore(t)
 	testutils.Clearstore(t, store)
-	//
-	//Create a new object and write to it.
-	//
+
+	// Create a new object and write to it.
 	obj, err := store.NewObject("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	f, err := obj.Open(cloudstorage.ReadWrite)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertT(t, f != nil, "the file was nil")
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, f, "the file was nil")
 
 	testcsv := "Year,Make,Model\n2003,VW,EuroVan\n2001,Ford,Ranger\n"
 
 	w := bufio.NewWriter(f)
 	n, err := w.WriteString(testcsv)
-	testutils.AssertEq(t, nil, err, "error. %d", n)
+	assert.Equal(t, nil, err, "error. %d", n)
 	w.Flush()
 
 	err = obj.Close()
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	//
 	// Ensure calling NewObject on an existing object returns an error,
 	// because the object exits.
-	//
 	obj2, err := store.NewObject("test.csv")
-	testutils.AssertEq(t, cloudstorage.ObjectExists, err, "error.")
-	testutils.AssertEq(t, nil, obj2, "object shoudl be nil.")
+	assert.Equal(t, cloudstorage.ObjectExists, err, "error.")
+	assert.Equal(t, nil, obj2, "object shoudl be nil.")
 
-	//
-	//Read the object back out of the cloud storage.
-	//
+	// Read the object back out of the cloud storage.
 	obj3, err := store.Get("test.csv")
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	f3, err := obj3.Open(cloudstorage.ReadOnly)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	bytes, err := ioutil.ReadAll(f3)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
-	testutils.AssertEq(t, testcsv, string(bytes))
+	assert.Equal(t, testcsv, string(bytes))
 }
 
 func TestReadWriteCloser(t *testing.T) {
@@ -321,18 +338,17 @@ func TestReadWriteCloser(t *testing.T) {
 	data := fmt.Sprintf("pid:%v:time:%v", os.Getpid(), time.Now().Nanosecond())
 
 	wc, err := store.NewWriter(object, nil)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 	buf1 := bytes.NewBufferString(data)
 	_, err = buf1.WriteTo(wc)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 	err = wc.Close()
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 
 	rc, err := store.NewReader(object)
-	testutils.AssertEq(t, nil, err, "error.")
+	assert.Equal(t, nil, err)
 	buf2 := bytes.Buffer{}
 	_, err = buf2.ReadFrom(rc)
-	testutils.AssertEq(t, nil, err, "error.")
-	testutils.AssertEq(t, data, buf2.String(), "round trip data don't match")
-
+	assert.Equal(t, nil, err)
+	assert.Equal(t, data, buf2.String(), "round trip data don't match")
 }
