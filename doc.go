5316f81165e241030dbfbeb601626a99136b584f
Aaron Raddon <araddon@gmail.com>
2017-12-28 19:28:53 -0800

    Doc

--- doc.go
+++ doc.go
@@ -1,28 +1,32 @@
-// Package cloudstorage is an interface to make Local, Google, s3 file storage
-// share a common interface to aid testing local as well as
-// running in the cloud.
-package cloudstorage
+/*
+Package cloudstorage is an interface to make Local, Google, s3 file storage
+share a common interface to aid testing local as well as
+running in the cloud.
 
-// Creating and iterating files from a cloudstorage.Store provider
-//
-// 		// This is an example of a local-storage (local filesystem) provider:
-//		config := &cloudstorage.Config{
-//			Type: localfs.StoreType,
-//			TokenSource:     localfs.AuthFileSystem,
-//			LocalFS:         "/tmp/mockcloud",
-//			TmpDir:          "/tmp/localcache",
-//		}
-//		store, _ := cloudstorage.NewStore(config)
-//
-//		// Create a query to define the search path
-//		q := cloudstorage.NewQuery("list-test/")
-//
-//		// Create an Iterator to list files
-//		iter := store.Objects(context.Background(), q)
-//		for {
-//			o, err := iter.Next()
-//			if err == iterator.Done {
-//				break
-//			}
-//			log.Println("found object %v", o.Name())
-//		}
+
+Creating and iterating files
+
+
+	// This is an example of a local-storage (local filesystem) provider:
+	config := &cloudstorage.Config{
+		Type: localfs.StoreType,
+		TokenSource:     localfs.AuthFileSystem,
+		LocalFS:         "/tmp/mockcloud",
+		TmpDir:          "/tmp/localcache",
+	}
+	store, _ := cloudstorage.NewStore(config)
+
+	// Create a query to define the search path
+	q := cloudstorage.NewQuery("list-test/")
+
+	// Create an Iterator to list files
+	iter := store.Objects(context.Background(), q)
+	for {
+		o, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		log.Println("found object %v", o.Name())
+	}
+*/
+package cloudstorage
