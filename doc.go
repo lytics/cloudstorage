76d9b0b5dd82d3b31c55d56cdcc158938c64f2be
Aaron Raddon <araddon@gmail.com>
2017-12-28 19:31:18 -0800

    Doc

--- doc.go
+++ doc.go
@@ -3,9 +3,17 @@ Package cloudstorage is an interface to make Local, Google, s3 file storage
 share a common interface to aid testing local as well as
 running in the cloud.
 
+The primary goal is to create a Store which is a common interface
+over each of the (google, s3, local-file-system, azure) etc file storage
+systems.   Then the methods (Query, filter, get, put) are common, as are
+the Files (Objects) themselves.  Writing code that supports multiple
+backends is now simple.
+
 
 Creating and iterating files
 
+In this example we are going to create a local-filesystem
+store.
 
 	// This is an example of a local-storage (local filesystem) provider:
 	config := &cloudstorage.Config{
