a0c46f6376251d1f73bd409e504163fc2879c334
eric <esniff@gmail.com>
2018-08-25 15:10:44 -0700

    get travis to run this again

--- go.test.sh
+++ go.test.sh
@@ -3,7 +3,9 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
+go test `go list ./... | grep localfs`
+
+for d in $(go list ./... | grep -v vendor  | sort -r); do
     go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
