d8b581070fa763ba07055ca402347b4d03a84534
Graham Forest <graham.forest@lytics.com>
2023-09-07 12:12:20 -0700

    Remove more

--- csbufio/writer.go
+++ csbufio/writer.go
@@ -13,7 +13,7 @@ type bufWriteCloser struct {
 	c   io.Closer
 }
 
-func OpenWriter(ctx context.Context, name string, enableCompression bool) (io.WriteCloser, error) {
+func OpenWriter(ctx context.Context, name string) (io.WriteCloser, error) {
 	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE, 0665)
 	if err != nil {
 		return nil, err
--- csbufio/writer_test.go
+++ csbufio/writer_test.go
@@ -1,7 +1,6 @@
 package csbufio
 
 import (
-	"bytes"
 	"context"
 	"io"
 	"testing"
@@ -31,54 +30,4 @@ func TestWriterContextDone(t *testing.T) {
 
 	err = wc.Close()
 	require.ErrorIs(t, err, context.Canceled)
-}
-
-func TestRoundtrip(t *testing.T) {
-	t.Parallel()
-
-	numBytes := 1000
-	testVal := bytes.Repeat([]byte("x"), numBytes)
-	pr, pw := bufpipe.New(nil)
-	wc := NewWriter(context.Background(), pw)
-
-	n, err := wc.Write(testVal)
-	require.NoError(t, err, "failed to write")
-	err = wc.Close()
-	require.NoError(t, err, "failed to close writer")
-	require.Equal(t, numBytes, n, "wrong number of uncompressed bytes written")
-	//require.Less(t, m.Length(), numBytes, "low entropy bytes did not compress")
-	err = pw.Close()
-	require.NoError(t, err, "failed to close writer, but the other one")
-
-	rc := NewReader(context.Background(), pr)
-	x, err := io.ReadAll(rc)
-	require.NoError(t, err, "failed to read")
-	//require.Equal(t, m.Length(), len(x), "wrong number of compressed bytes read")
-	err = rc.Close()
-	require.NoError(t, err, "failed to close reader")
-	require.Equal(t, testVal, x)
-}
-
-func TestRoundtripWithCompression(t *testing.T) {
-	t.Parallel()
-
-	numBytes := 1000
-	testVal := bytes.Repeat([]byte("x"), numBytes)
-	pr, pw := bufpipe.New(nil)
-	wc := NewWriter(context.Background(), pw)
-
-	n, err := wc.Write(testVal)
-	require.NoError(t, err, "failed to write")
-	err = wc.Close()
-	require.NoError(t, err, "failed to close writer")
-	require.Equal(t, numBytes, n, "wrong number of uncompressed bytes written")
-	err = pw.Close()
-	require.NoError(t, err, "failed to close writer, but the other one")
-
-	rc := NewReader(context.Background(), pr)
-	x, err := io.ReadAll(rc)
-	require.NoError(t, err, "failed to read")
-	err = rc.Close()
-	require.NoError(t, err, "failed to close reader")
-	require.Equal(t, testVal, x)
 }
