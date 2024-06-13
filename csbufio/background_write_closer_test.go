eec8e0a3869faad0b123766cc5028f6aa55e0a11
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-06-13 13:39:02 +0200

    port azure write closer to a more generic background write closer

---
+++ csbufio/background_write_closer_test.go
@@ -0,0 +1,102 @@
+package csbufio_test
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/lytics/cloudstorage/csbufio"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBackgroundWriteCloser(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		label         string
+		mustCancelCtx bool
+		closeErrMsg   string
+	}{
+		{
+			label:         "test success on write and close",
+			mustCancelCtx: false,
+		},
+		{
+			label:         "test success on write but close return error",
+			mustCancelCtx: true,
+			closeErrMsg:   "error from background job: context canceled",
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.label, func(t *testing.T) {
+			t.Parallel()
+
+			ctx, cancel := context.WithCancel(context.Background())
+			t.Cleanup(cancel)
+
+			if tc.mustCancelCtx {
+				cancel()
+			}
+
+			var dataSentToCloud []byte
+			wc := csbufio.NewBackgroundWriteCloser(ctx, func(ctx context.Context, rc io.ReadCloser) error {
+				defer rc.Close()
+
+				data, err := io.ReadAll(rc)
+				if err != nil {
+					return err
+				}
+
+				dataSentToCloud = data
+
+				return ctx.Err()
+			})
+
+			_, err := wc.Write([]byte("foo"))
+			require.NoError(t, err)
+
+			err = wc.Close()
+			if tc.closeErrMsg != "" {
+				require.EqualError(t, err, tc.closeErrMsg)
+
+				return
+			}
+
+			require.NoError(t, err)
+
+			assert.Equal(t, []byte("foo"), dataSentToCloud)
+		})
+	}
+}
+
+func TestBackgroundWriteCloserCheckWriteAfterClose(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	wc := csbufio.NewBackgroundWriteCloser(ctx, func(ctx context.Context, rc io.ReadCloser) error {
+		defer rc.Close()
+
+		return nil
+	})
+
+	require.NoError(t, wc.Close())
+
+	_, err := wc.Write([]byte("foo"))
+	require.EqualError(t, err, "writer already closed")
+}
+
+func TestBackgroundWriteCloserCheckMultipleCloses(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	wc := csbufio.NewBackgroundWriteCloser(ctx, func(ctx context.Context, rc io.ReadCloser) error {
+		defer rc.Close()
+
+		return nil
+	})
+
+	require.NoError(t, wc.Close())
+
+	require.NoError(t, wc.Close(), "close an already closed wc should not trigger an error")
+}
